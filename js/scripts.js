f23e50e360747690a968183e27ae64a80dc43af6
Lee Pedersen <leepedersenr@gmail.com>
2019-11-08 11:37:34 -0800

    create calcPrice prototype

--- js/scripts.js
+++ js/scripts.js
@@ -10,18 +10,49 @@ Pizza.prototype.setSize = function(size) {
   this.size = size;
 }
 
+Pizza.prototype.calcPrice = function(toppingsNumber, size) {
+  var price = "";
+  if (size === "small potatoes") {
+    if (toppingsNumber < 3) {
+      price = 8;
+    } else if (toppingsNumber < 6) {
+      price = 9;
+    } else {
+      price = 10;
+    }
+  } else if (size === "happy medium") {
+    if (toppingsNumber < 3) {
+      price = 10;
+    } else if (toppingsNumber < 6) {
+      price = 11;
+    } else {
+      price = 12;
+    }
+  } else if (size === "by and large") {
+    if (toppingsNumber < 3) {
+      price = 12;
+    } else if (toppingsNumber < 6) {
+      price = 13;
+    } else {
+      price = 14;
+    }
+  }
+  return price;
+}
+
 var newPizza = new Pizza();
 
 function showPizza(pizzaShow) {
   $("#show-pizza").show();
   $("#show-size").text(newPizza.size);
 
+  $("#show-price").text(newPrice);
+
   for (var i = 0; i < 7; i++) {
     if (newPizza.toppings[i]) {
       $("#show-toppings").append("<li>" + newPizza.toppings[i] + "</li>");
     }
   }
-  console.log(newPizza.toppings[0]);
 }
 
 $(document).ready(function() {
@@ -46,6 +77,8 @@ $(document).ready(function() {
     newPizza.addTopping(topping5);
     newPizza.addTopping(topping6);
 
+    newPrice = newPizza.calcPrice(newPizza.toppings.length, inputSize);
+console.log(newPizza.toppings);
     showPizza();
 
   });
