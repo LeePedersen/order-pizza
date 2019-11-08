5aa93f3ec053209ca64add4848654e6b4816d055
Lee Pedersen <leepedersenr@gmail.com>
2019-11-08 10:18:44 -0800

    read all checked boxes into array and show array

--- js/scripts.js
+++ js/scripts.js
@@ -13,22 +13,32 @@ Pizza.prototype.setSize = function(size) {
 var newPizza = new Pizza();
 
 function showPizza() {
-  $("#show-pizza").text(newPizza);
-  console.log(newPizza);
+  $("#show-pizza").text(newPizza.toppings);
+  console.log(newPizza.toppings);
 }
 
 $(document).ready(function() {
   $("form#new-pizza").submit(function(event) {
     event.preventDefault();
-    var topping1 = $("#topping1").val();
-    var topping2 = $("#topping2").val();
-    var topping3 = $("#topping3").val();
-    var topping4 = $("#topping4").val();
-    var topping5 = $("#topping5").val();
-    var topping6 = $("#topping6").val();
-    // put toppings in if loop
 
-    newPizza.addTopping(topping1);
+
+
+    var topping1 = $("input:checkbox[id=topping1]:checked").val();
+    var topping2 = $("input:checkbox[id=topping2]:checked").val();
+    var topping3 = $("input:checkbox[id=topping3]:checked").val();
+    var topping4 = $("input:checkbox[id=topping4]:checked").val();
+    var topping5 = $("input:checkbox[id=topping5]:checked").val();
+    var topping6 = $("input:checkbox[id=topping6]:checked").val();
+  //  put toppings in for loop
+
+console.log(topping1);
+
+      newPizza.addTopping(topping1);
+      newPizza.addTopping(topping2);
+      newPizza.addTopping(topping3);
+      newPizza.addTopping(topping4);
+      newPizza.addTopping(topping5);
+      newPizza.addTopping(topping6);
 
     showPizza();
 
